88e64c23c7b867a77cd64027a2a0362b1298ee9a
jcardar <jcardar@umich.edu>
2022-09-27 23:17:53 -0400

    First naive alt tag checker. Loops are definitely not efficient, need to rework that

--- tourney_import.py
+++ tourney_import.py
@@ -40,9 +40,7 @@ def get_top_8(event_id, authToken):
   result = result.split(']}}}')[0]
   return result
 
-## Make sure to run `pip install graphqlclient`
-
-authToken = None #BE SURE TO ENTER YOUR AUTHTOKEN FROM START.GG
+authToken = '9292bda8f96a5b1f2f425c546de5ae92' #BE SURE TO ENTER YOUR AUTHTOKEN FROM START.GG
 apiVersion = 'alpha'
 
 
@@ -50,8 +48,8 @@ tourn_database  = open('database_tournaments.csv', 'w+')
 tourn_writer    = csv.writer(tourn_database); tourn_writer.writerow(['Tournament Name', 'Date', 'Number of Entrants', 'Tournament URL', 'Top-8'])
 player_database = open('database_players.csv', 'w+')
 player_writer   = csv.writer(player_database); player_writer.writerow(['Tag', 'Known Alts', 'Tournament: Placing'])
-head2head_sets  = open('database_head2heads.csv', 'w+')
-head2head_games = open('database_head2heads_games.csv', 'w+')
+head2head_sets  = open('database_head2heads.csv', 'w+') #work in progress
+head2head_games = open('database_head2heads_games.csv', 'w+') #work in progress
 head2head_sets_writer  = csv.writer(head2head_sets)
 head2head_games_writer = csv.writer(head2head_games)
 
@@ -61,6 +59,7 @@ client = GraphQLClient('https://api.start.gg/gql/alpha')
 client.inject_token('Bearer ' + authToken)
 tournaments = json.load(open('tournaments.json')) #reads list of tournaments from .json file. Only URL is required, but name of tournament may be good for organizing the json file.
 knownalts   = json.load(open('knownAlts.json'))
+alt_tags = [alt_dict["Alt"].lower() for alt_dict in knownalts]
 broke_tourneys = []
 analyzed_tourneys = []
 players = {}
@@ -103,15 +102,29 @@ for tourney in tournaments:
     for set in sets:
       entrant1 = set['entrant1Players'][0]['playerTag'].split(' | ')[-1].strip().lower()
       entrant2 = set['entrant2Players'][0]['playerTag'].split(' | ')[-1].strip().lower()
-      
+      # print(knownalts)
       if (set['completed'] and set['entrant1Score'] >= 0 and set['entrant2Score'] >= 0):
+        # print(entrant1)
         if (entrant1 == None or entrant2 == None):
           continue
-        if entrant1 not in players:
-          if "Tag" in players:
-            # if entrant1 CHECK IF ENTRANT1'S TAG IS IN THE KNOWN ALT LIST BEFORE ADDING IT TO THE PLAYERS LIST
-            players["Tag"].append(entrant1)
+        if entrant1 not in players.keys():
+          if entrant1 not in alt_tags:
+            players[entrant1] = [] #Thinking about what best to put here! 9/23/22
+          elif entrant1 in alt_tags:#tag is a known alt, check if actual player is in the database already:
+            for knowntag_dict in knownalts: #THERE'S got to be a more efficient way to do this, but my brain is tired :)
+              if entrant1 == knowntag_dict["Alt"].lower():
+                if knowntag_dict["Tag"] not in players:
+                  players[knowntag_dict["Tag"].lower()] = []
           else:
-            players["Tag"] = [entrant1]
-        if entrant2 not in players:
-          players["Tag"].append(entrant2)
+            print(f"DID NOT KNOW HOW TO PROCESS PLAYER {entrant1} FOR TOURNAMENT {tourn_w_brack['name']}")
+
+        if entrant2 not in players.keys():
+          if entrant2 not in alt_tags:
+            players[entrant2] = [] #Thinking about what best to put here! 9/23/22
+          elif entrant2 in alt_tags:#tag is a known alt, check if actual player is in the database already:
+            for knowntag_dict in knownalts: #THERE'S got to be a more efficient way to do this, but my brain is tired :)
+              if entrant2 == knowntag_dict["Alt"].lower():
+                if knowntag_dict["Tag"] not in players:
+                  players[knowntag_dict["Tag"].lower()] = []
+          else:
+            print(f"DID NOT KNOW HOW TO PROCESS PLAYER {entrant2} FOR TOURNAMENT {tourn_w_brack['name']}")
